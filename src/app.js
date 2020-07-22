a53b67273080c38d042adfc4bf2ffd352727a8bd
ashleybiermann <ashleybiermann@gmail.com>
2020-07-22 00:34:04 -0700

    still need to add form hooks. Tried to get testing started, but couldn't seem to find the test file....

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/header/header.js';
-import ToDo from './components/todo/todo-refactor-plus-connected.js';
+import ToDo from './components/todo/todo.js';
 
 export default class App extends React.Component {
   render() {
